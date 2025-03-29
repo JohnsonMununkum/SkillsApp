788ca87dbf26bc59c5cb7aa8e61a9f104c70f343
Johnson <G00419319@atu.ie>
2025-03-29 15:17:59 +0000

    login.ts configred

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule,FormGroup} from '@angular/forms';
+//for authentication login through firebase
+//router for navigation
+import { Router } from '@angular/router';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
+  loginForm!: FormGroup;
+  //variables
+  email: string = '';
+  password: string = '';
 
-  constructor() { }
+  //constructor for the ifrebase authentication 
+  //and for the router for navigation
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) { }
 
+  async login() {
+    //gets the users email and password input
+    //if correct user is logged in
+    //and is then navigated to the home page
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        this.auth,
+        this.email,
+        this.password
+      );
+      console.log('Logged in:', userCredential.user?.email);
+      this.router.navigate(['/home']);
+      //if details are not correct an error is displayed 
+    } catch (error) {
+      console.error('Error logging in:', error);
+    }
+  }
+  
+  
   ngOnInit() {
   }
 
+
 }
+
+
